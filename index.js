fb7f905d81ea29efa9197ee9ca53081ee25c7efb
Ariana Silva <aariana.martins@hotmail.com>
2024-01-18 11:12:08 -0300

    update colors

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const colorThreeButton = document.getElementById("colorThree");
 const corpo = document.querySelector("body");
 
 colorOneButton.addEventListener("click", () => {
-    corpo.style.backgroundColor = "#E01180";
-  });
-colorTwoButton.addEventListener("click", () => {
     corpo.style.backgroundColor = "#B511E0";
   });
-colorThreeButton.addEventListener("click", () => {
+colorTwoButton.addEventListener("click", () => {
     corpo.style.backgroundColor = "#0FA9D9";
+  });
+colorThreeButton.addEventListener("click", () => {
+    corpo.style.backgroundColor = "#E9BF11";
   });
